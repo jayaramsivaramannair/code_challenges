36935ac7518b61ea266d5d012ddcb3a37bfd6922
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2021-07-07 19:54:06 -0400

    Added a method to the Linked List class to call print method directly on Linked List

--- day12/20210707a.py
+++ day12/20210707a.py
@@ -33,6 +33,20 @@ class LinkedList:
             # If no reference to a head was passed. It means that the new node itself is the head.
             self.head = new_node
 
+    # The below will enable us to iterate over element of the linked list using for _ in _ syntax
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+
+    # The below will enable us to call the print() method directly on the linked list
+    def __repr__(self):
+        nodes = []
+        for node in self:
+            nodes.append(str(node.data))
+        return " -> ".join(nodes)
+
 
 # Creating a new node for the linked list
 a = LinkedListNode(1)
@@ -41,12 +55,17 @@ a = LinkedListNode(1)
 my_ll = LinkedList(a)
 my_ll.append(2)
 my_ll.append(3)
+my_ll.append(4)
+my_ll.append(5)
 
 # Below will print the value of a which is '1' because it is the head in this Linked List Structure
-print(my_ll.head.data)
+# print(my_ll.head.data)
 
 # This will print 2 as my_ll.head.next is pointing to the next element in the list which is '2'
-print(my_ll.head.next.data)
+# print(my_ll.head.next.data)
 
 # This will print 3
-print(my_ll.head.next.next.data)
+# print(my_ll.head.next.next.data)
+
+
+print(my_ll)
